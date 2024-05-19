5d6fb4e3d58ad2fa82d80948f5b7a792ddd1af68
Tsyhaniuk Kateryna <kyzkakatryska@gmail.com>
2024-05-20 00:46:17 +0300

    add home work

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, 0)}`; //функція для генерування випадкового кольору
+}
+//знаходимо кнопку і додаємо спостерігача який при кліку буде визивати функцію
+const changeRbgBtn = document.querySelector('.change-color');
+changeRbgBtn.addEventListener('click', changeRbg);
+
+function changeRbg() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor; //змінюємо колір фону на новий колір
+  document.querySelector('.color').textContent = newColor; //змінюємо текстовий вміст елемента на новий колір
 }
