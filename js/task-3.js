e4b3d3341da68848f55f09b8d4a2a420aedb7ac6
Tsyhaniuk Kateryna <kyzkakatryska@gmail.com>
2024-05-20 22:16:38 +0300

    apdate task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,19 @@
 'use strict';
-//знаходимо улументи у DOM
 
-const inputElem = document.querySelector('name-input');
-const outputElem = document.querySelector('name-output');
+const inputElem = document.querySelector('#name-input');
+const outputElem = document.querySelector('#name-output');
 
-// функція для обрізки пробілів
 function trimInputValue(value) {
   return value.trim();
 }
 
-// функція для отримання тексту для відображення
-
 function getDisplayText(value) {
-  return value === '' ? 'Anonymous' : value; //якщо value є порожнім рядком функція повертає Аnonymous, в іншому випадку повертає саме значення value
+  return value === '' ? 'Anonymous' : value;
 }
-
-// функція для оновлення тексту в елементі вводу
 
 function updateOutput(event) {
   const trimmedValue = trimInputValue(event.target.value);
   outputElem.textContent = getDisplayText(trimmedValue);
 }
-
-//даємо доручення на обробку події
 
 inputElem.addEventListener('input', updateOutput);
